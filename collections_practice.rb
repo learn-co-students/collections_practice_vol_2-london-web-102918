91552921074beb9e519de343c9528188804a9aef
Paul Clark <clark.paul123@gmail.com>
2018-10-23 10:03:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|i| i.start_with?("r")}
+end
+
+def contain_a(array)
+  array.select {|i| i.include?("a")}
+end
+
+def first_wa(array)
+  array.find {|element| element[0..1].include?("wa")}
+end
+
+def remove_non_strings(array)
+  array.select {|i| i.is_a? String}
+end
+
+def count_elements(array)
+  array.uniq.each do |name1| count = 0 
+    array.each do |name2| if name1 == name2
+      count +=1
+      end
+    end
+      name1[:count] = count
+  end
+end
+
+def merge_data(keys, data)
+  merged_data =[] 
+  keys.each
+end
+
+def find_cool(cool)
+  cool.select {|element| element.any? {|key, value| value == "cool"}}
+end 
+
+def organize_schools(schools)
+  locations = {}
+  schools.collect {|key, value| locations[value[:location]] = []}
+    locations.each do |key, value| schools.each do |key1, value1| 
+      if key == value1[:location] 
+        value.push(key1)
+      end
+    end
+  end
+end 
